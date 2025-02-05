e660a09cc0b2223ed064b37813e4ad433360c84c
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2025-02-05 23:13:17 +0300

    Initial

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestingBackend.Data;
+using TestingBackend.Models;
+
+namespace TestingBackend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return product;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostProduct(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+
+        return Ok($"Продукт {product.Name} добавлен успешно.");
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestingBackend.Models;
+
+public class Product
+{
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+}
