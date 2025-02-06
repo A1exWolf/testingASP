ae6a9ed5e79aa9d0571a8fe56dc76a51d665084f
Dont't worry <81536837+A1exWolf@users.noreply.github.com>
2025-02-06 12:34:22 +0300

    [refactor] Добавил другие http запросы

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TestingBackend.Data;
 using TestingBackend.Models;
 
@@ -13,6 +14,12 @@ public class ProductsController : ControllerBase
     public ProductsController(AppDbContext context)
     {
         _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        return await _context.Products.ToListAsync();
     }
 
     [HttpGet("{id}")]
@@ -33,5 +40,41 @@ public class ProductsController : ControllerBase
         await _context.SaveChangesAsync();
 
         return Ok($"Продукт {product.Name} добавлен успешно.");
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutProduct(int id, Product product)
+    {
+        if (id != product.Id)
+            return BadRequest();
+
+        _context.Entry(product).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!_context.Products.Any(e => e.Id == id))
+                return NotFound();
+            else
+                throw;
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null)
+            return NotFound();
+
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
