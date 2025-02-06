4a535828b2288adf0d62fddf2710fc923fe260e2
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2025-02-06 22:14:42 +0300

    [refactor] Добавил logger на запросы

---
+++ Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace TestingBackend.Middleware;
+
+public class RequestLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<RequestLoggingMiddleware> _logger;
+
+    public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        _logger.LogInformation($"HTTP request received: {context.Request.Method} {context.Request.Path}");
+        await _next(context);
+        _logger.LogInformation($"HTTP response sent: {context.Response.StatusCode}");
+    }
+}
